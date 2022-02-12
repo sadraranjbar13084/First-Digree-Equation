870a5fbf75f8067259b905ec8896ce0baa08f107
sadraranjbar13084 <88903602+sadraranjbar13084@users.noreply.github.com>
2022-02-12 19:02:46 +0330

    Add files via upload

---
+++ First Degree equation/First Degree equation/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace First_Degree_equation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblequ = new System.Windows.Forms.Label();
+            this.lblplus = new System.Windows.Forms.Label();
+            this.lblx = new System.Windows.Forms.Label();
+            this.btnabout = new System.Windows.Forms.Button();
+            this.btnsubmit = new System.Windows.Forms.Button();
+            this.lblabout = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblans = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.NumericUpDown();
+            this.num1 = new System.Windows.Forms.NumericUpDown();
+            this.num3 = new System.Windows.Forms.NumericUpDown();
+            this.txtanswer = new System.Windows.Forms.TextBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            ((System.ComponentModel.ISupportInitialize)(this.num2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.helpProvider1.SetHelpKeyword(this.label1, resources.GetString("label1.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.label1, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("label1.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.label1, resources.GetString("label1.HelpString"));
+            this.label1.Name = "label1";
+            // 
+            // lblequ
+            // 
+            resources.ApplyResources(this.lblequ, "lblequ");
+            this.helpProvider1.SetHelpKeyword(this.lblequ, resources.GetString("lblequ.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.lblequ, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("lblequ.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.lblequ, resources.GetString("lblequ.HelpString"));
+            this.lblequ.Name = "lblequ";
+            // 
+            // lblplus
+            // 
+            resources.ApplyResources(this.lblplus, "lblplus");
+            this.helpProvider1.SetHelpKeyword(this.lblplus, resources.GetString("lblplus.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.lblplus, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("lblplus.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.lblplus, resources.GetString("lblplus.HelpString"));
+            this.lblplus.Name = "lblplus";
+            // 
+            // lblx
+            // 
+            resources.ApplyResources(this.lblx, "lblx");
+            this.helpProvider1.SetHelpKeyword(this.lblx, resources.GetString("lblx.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.lblx, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("lblx.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.lblx, resources.GetString("lblx.HelpString"));
+            this.lblx.Name = "lblx";
+            // 
+            // btnabout
+            // 
+            resources.ApplyResources(this.btnabout, "btnabout");
+            this.helpProvider1.SetHelpKeyword(this.btnabout, resources.GetString("btnabout.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.btnabout, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("btnabout.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.btnabout, resources.GetString("btnabout.HelpString"));
+            this.btnabout.Name = "btnabout";
+            this.btnabout.UseVisualStyleBackColor = true;
+            this.btnabout.Click += new System.EventHandler(this.btnabout_Click);
+            // 
+            // btnsubmit
+            // 
+            resources.ApplyResources(this.btnsubmit, "btnsubmit");
+            this.helpProvider1.SetHelpKeyword(this.btnsubmit, resources.GetString("btnsubmit.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.btnsubmit, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("btnsubmit.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.btnsubmit, resources.GetString("btnsubmit.HelpString"));
+            this.btnsubmit.Name = "btnsubmit";
+            this.btnsubmit.UseVisualStyleBackColor = true;
+            this.btnsubmit.Click += new System.EventHandler(this.btnsubmit_Click);
+            // 
+            // lblabout
+            // 
+            resources.ApplyResources(this.lblabout, "lblabout");
+            this.helpProvider1.SetHelpKeyword(this.lblabout, resources.GetString("lblabout.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.lblabout, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("lblabout.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.lblabout, resources.GetString("lblabout.HelpString"));
+            this.lblabout.Name = "lblabout";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.helpProvider1.SetHelpKeyword(this.label3, resources.GetString("label3.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.label3, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("label3.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.label3, resources.GetString("label3.HelpString"));
+            this.label3.Name = "label3";
+            // 
+            // lblans
+            // 
+            resources.ApplyResources(this.lblans, "lblans");
+            this.helpProvider1.SetHelpKeyword(this.lblans, resources.GetString("lblans.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.lblans, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("lblans.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.lblans, resources.GetString("lblans.HelpString"));
+            this.lblans.Name = "lblans";
+            // 
+            // num2
+            // 
+            resources.ApplyResources(this.num2, "num2");
+            this.helpProvider1.SetHelpKeyword(this.num2, resources.GetString("num2.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.num2, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("num2.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.num2, resources.GetString("num2.HelpString"));
+            this.num2.Maximum = new decimal(new int[] {
+            50000000,
+            0,
+            0,
+            0});
+            this.num2.Minimum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            -2147483648});
+            this.num2.Name = "num2";
+            // 
+            // num1
+            // 
+            resources.ApplyResources(this.num1, "num1");
+            this.helpProvider1.SetHelpKeyword(this.num1, resources.GetString("num1.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.num1, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("num1.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.num1, resources.GetString("num1.HelpString"));
+            this.num1.Maximum = new decimal(new int[] {
+            5000000,
+            0,
+            0,
+            0});
+            this.num1.Minimum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            -2147483648});
+            this.num1.Name = "num1";
+            // 
+            // num3
+            // 
+            resources.ApplyResources(this.num3, "num3");
+            this.helpProvider1.SetHelpKeyword(this.num3, resources.GetString("num3.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.num3, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("num3.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.num3, resources.GetString("num3.HelpString"));
+            this.num3.Maximum = new decimal(new int[] {
+            50000000,
+            0,
+            0,
+            0});
+            this.num3.Minimum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            -2147483648});
+            this.num3.Name = "num3";
+            // 
+            // txtanswer
+            // 
+            resources.ApplyResources(this.txtanswer, "txtanswer");
+            this.txtanswer.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.helpProvider1.SetHelpKeyword(this.txtanswer, resources.GetString("txtanswer.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this.txtanswer, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("txtanswer.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this.txtanswer, resources.GetString("txtanswer.HelpString"));
+            this.txtanswer.Name = "txtanswer";
+            // 
+            // helpProvider1
+            // 
+            resources.ApplyResources(this.helpProvider1, "helpProvider1");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.txtanswer);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.lblans);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblabout);
+            this.Controls.Add(this.btnsubmit);
+            this.Controls.Add(this.btnabout);
+            this.Controls.Add(this.lblplus);
+            this.Controls.Add(this.lblx);
+            this.Controls.Add(this.lblequ);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.helpProvider1.SetHelpKeyword(this, resources.GetString("$this.HelpKeyword"));
+            this.helpProvider1.SetHelpNavigator(this, ((System.Windows.Forms.HelpNavigator)(resources.GetObject("$this.HelpNavigator"))));
+            this.helpProvider1.SetHelpString(this, resources.GetString("$this.HelpString"));
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.num2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblequ;
+        private System.Windows.Forms.Label lblplus;
+        private System.Windows.Forms.Label lblx;
+        private System.Windows.Forms.Button btnabout;
+        private System.Windows.Forms.Button btnsubmit;
+        private System.Windows.Forms.Label lblabout;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblans;
+        private System.Windows.Forms.NumericUpDown num2;
+        private System.Windows.Forms.NumericUpDown num1;
+        private System.Windows.Forms.NumericUpDown num3;
+        private System.Windows.Forms.TextBox txtanswer;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
---
+++ First Degree equation/First Degree equation/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First_Degree_equation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnabout_Click(object sender, EventArgs e)
+        {
+            lblabout.Text = "طراح و برنامه نویس: صدرا رنجبر" +
+                "            " +
+                "تاریخ : 2 فوریه 2022";
+        }
+
+        private void btnsubmit_Click(object sender, EventArgs e)
+        {
+            double num4 = (double)(num3.Value - num2.Value);
+            double answer = num4 / (double)num1.Value;
+            string answer2 = Convert.ToString(answer);
+            txtanswer.Text = answer2;
+        }
+    }
+}
